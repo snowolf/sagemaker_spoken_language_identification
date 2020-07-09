9c4b825dddaff4f9a524af86bebc43795fbdf42e
snowolf <wangshishuai1987@gmail.com>
2020-07-09 15:15:35 +0000

    first commit

--- 3-inference/slr/jobs/.ipynb_checkpoints/pipeline-checkpoint.py
+++
@@ -1,8 +0,0 @@
-class Pipeline:
-    def __init__(self, jobs, context=None):
-        self.jobs = jobs
-        self.context = context or {}
-
-    def execute(self):
-        for job in self.jobs:
-            job.execute(self.context)
